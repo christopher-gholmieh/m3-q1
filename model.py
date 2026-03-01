f09b1b091b40b44cf6ac1dea6b0094b13cc42f38
christopher-gholmieh <gholmieh.chris@gmail.com>
2026-02-28 20:40:48 -0800

    [+] Model!

--- model.py
+++ model.py
@@ -33,7 +33,7 @@ y = dataframe["expense"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # Model:
-model = RandomForestRegressor(n_estimators=200)
+model = RandomForestRegressor(n_estimators=100)
 
 # Fit:
 model.fit(X_train, y_train)
